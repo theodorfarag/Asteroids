8b0ed359b8730515753f3ff626a7d52107ca5be7
theodorfarag <Theodorfarag@gmail.com>
2026-01-14 16:45:16 -0500

    Exit event added to the screen

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}\n' + 
           f'Screen height: {SCREEN_HEIGHT}')
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
